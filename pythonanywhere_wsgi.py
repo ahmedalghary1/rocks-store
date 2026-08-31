8c169c9ee7b5c46a89275b05ce5535124046c928
ahmedalghary1 <ahmedalghary1@gmail.com>
2026-08-31 15:26:03 +0300

    Add WSGI configuration for the ROCKS store on PythonAnywhere

---
+++ pythonanywhere_wsgi.py
@@ -0,0 +1,23 @@
+"""WSGI configuration for the ROCKS store on PythonAnywhere.
+
+Copy this file's contents into the WSGI configuration file shown on the
+PythonAnywhere Web tab.
+"""
+
+import os
+import sys
+
+
+PROJECT_ROOT = "/home/rocksev/rocks-store"
+
+if PROJECT_ROOT not in sys.path:
+    sys.path.insert(0, PROJECT_ROOT)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DEBUG", "False")
+os.environ.setdefault("ALLOWED_HOSTS", "rocksev.pythonanywhere.com")
+
+from django.core.wsgi import get_wsgi_application
+
+
+application = get_wsgi_application()
